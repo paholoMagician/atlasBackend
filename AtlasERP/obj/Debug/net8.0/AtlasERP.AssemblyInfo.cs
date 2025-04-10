42f012e592d76e809f2600f4590f801c8b4afab4
AndreMach90 <144734631+AndreMach90@users.noreply.github.com>
2025-04-10 16:56:28 -0500

    El vuelto se agregado

--- AtlasERP/obj/Debug/net8.0/AtlasERP.AssemblyInfo.cs
+++ AtlasERP/obj/Debug/net8.0/AtlasERP.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AtlasERP")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5069dfde7e1e93aeb6a836d844d0665dd75be9b3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f51ba51d87ff96c3666055b58715290f3a199917")]
 [assembly: System.Reflection.AssemblyProductAttribute("AtlasERP")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AtlasERP")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
